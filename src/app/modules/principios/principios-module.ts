ac91a336f7749f6921fed6cef2ac0d50a7323e14
Camilo Suarez <juan.suarez04@uceva.edu.co>
2026-09-02 19:07:02 -0500

    feat: scaffolding del módulo principios de arquitectura con routing

---
+++ src/app/modules/principios/principios-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PrincipiosRoutingModule } from './principios-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PrincipiosRoutingModule
+  ]
+})
+export class PrincipiosModule { }
