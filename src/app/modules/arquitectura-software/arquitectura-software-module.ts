fd3774fa33e1f63ba2bd964dbaa9b9e81aa86d06
Sebastian <sebastian.morales01@uceva.edu.co>
2026-09-02 19:12:52 -0500

    feat: scaffolding del módulo arquitectura de software con routing

---
+++ src/app/modules/arquitectura-software/arquitectura-software-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArquitecturaSoftwareRoutingModule } from './arquitectura-software-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ArquitecturaSoftwareRoutingModule
+  ]
+})
+export class ArquitecturaSoftwareModule { }
